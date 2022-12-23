80716f5101ef6deae1ac3416e5ebad64dc2acee9
Marcin Sławicz <marcin.slawicz@gmail.com>
2022-12-23 15:08:20 +0100

    battery level settings test

--- Core/Src/main_app.cpp
+++ Core/Src/main_app.cpp
@@ -6,10 +6,22 @@
  */
 
 #include "main_app_api.h"
+#include "main.h"
+#include "custom_stm.h"
 
 void mainApp()
 {
-    //uint8_t batteryLevel = 65;
-    //Custom_STM_App_Update_Char(CUSTOM_STM_BATLVL, &batteryLevel);
+    static uint32_t cnt = 0;
+    static uint8_t batteryLevel = 99;
+    if(cnt++ > 100000)
+    {
+        HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+        Custom_STM_App_Update_Char(CUSTOM_STM_BATLVL, &batteryLevel);
+        if(batteryLevel-- == 0)
+        {
+            batteryLevel = 99;
+        }
+        cnt = 0;
+    }
 }
 
