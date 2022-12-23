628957cc49e0ee302511c67f5c5bfaa27379f5a8
Marcin Sławicz <marcin.slawicz@gmail.com>
2022-12-23 14:42:48 +0100

    main_app files added

---
+++ Core/Inc/main_app_api.h
@@ -0,0 +1,21 @@
+/*
+ * main_app_api.h
+ *
+ *  Created on: Dec 23, 2022
+ *      Author: marci
+ */
+
+#ifndef INC_MAIN_APP_API_H_
+#define INC_MAIN_APP_API_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void mainApp();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* INC_MAIN_APP_API_H_ */
---
+++ Core/Src/main_app.cpp
@@ -0,0 +1,15 @@
+/*
+ * main_app.cpp
+ *
+ *  Created on: Dec 23, 2022
+ *      Author: marci
+ */
+
+#include "main_app_api.h"
+
+void mainApp()
+{
+    //uint8_t batteryLevel = 65;
+    //Custom_STM_App_Update_Char(CUSTOM_STM_BATLVL, &batteryLevel);
+}
+
